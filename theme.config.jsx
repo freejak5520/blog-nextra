37e8676729424ee15c86416da1cb3cd9d8c49b80
Jake Lee <freejak5520@gmail.com>
2024-04-15 14:12:57 +0900

    푸터 닉네임, 깃헙 주소 변경

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,7 +3,7 @@ const config = {
     return (
       <small style={{ display: "flex", justifyContent: "space-between" }}>
         <div>
-          © <time>{new Date().getFullYear()}.</time> smoothsquid. All rights
+          © <time>{new Date().getFullYear()}.</time> freejak5520. All rights
           reserved.
         </div>
       </small>
@@ -18,12 +18,12 @@ const config = {
       {meta.author && <meta name="author" content={meta.author} />}
     </>
   ),
-  readMore: "Read More →",
+  readMore: "더보기 →",
   postFooter: null,
   darkMode: false,
   navs: [
     {
-      url: "https://github.com/smoothsquid",
+      url: "https://github.com/freejak5520",
       name: "Github",
     },
   ],
