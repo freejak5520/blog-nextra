c8525561a761e6b251d3aedad505347de68b0a1f
Jake Lee <freejak5520@gmail.com>
2024-04-16 22:20:19 +0900

    GTM 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-// import "@/styles/globals.css";
+import { TrackingHeadScript } from "@phntms/next-gtm";
 import type { AppProps } from "next/app";
 
+const GA_ID = "GTM-MLQ58GB3";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <TrackingHeadScript id={GA_ID} isGTM={true} />
+      <Component {...pageProps} />
+    </>
+  );
 }
