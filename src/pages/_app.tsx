b4c0c9157d3e94d94afecd2717324f1d44de98d1
Jake Lee <freejak5520@gmail.com>
2024-04-16 22:21:47 +0900

    vercel Analytics 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { TrackingHeadScript } from "@phntms/next-gtm";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 const GA_ID = "GTM-MLQ58GB3";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Analytics />
       <TrackingHeadScript id={GA_ID} isGTM={true} />
       <Component {...pageProps} />
     </>
