5eb8fb56bfae911a1eae5745df707dd2c6caf7fd
Jake Lee <freejak5520@gmail.com>
2024-05-03 18:38:41 +0900

    footer marginTop 추가

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 const config = {
   footer: (() => {
     return (
-      <small style={{ display: "flex", justifyContent: "space-between" }}>
+      <small style={{ display: "flex", justifyContent: "space-between", marginTop: "8rem" }}>
         <div>
           © <time>{new Date().getFullYear()}.</time> freejak5520. All rights
           reserved.
@@ -30,53 +30,3 @@ const config = {
 };
 
 export default config;
-
-// import Cusdis from 'nextra-theme-blog/cusdis'
-
-// /* eslint sort-keys: error */
-// export default {
-//   comments: <Cusdis />,
-//   components: {
-//     h1: ({ children }) => (
-//       <h1
-//         style={{
-//           WebkitBackgroundClip: 'text',
-//           WebkitTextFillColor: 'transparent',
-//           backgroundClip: 'text',
-//           backgroundImage: 'linear-gradient(90deg,#7928CA,#FF0080)'
-//         }}
-//       >
-//         {children}
-//       </h1>
-//     )
-//   },
-//   cusdis: {
-//     appId: 'a2d11511-7012-4254-9483-cb49c8f4dfe8'
-//   },
-//   darkMode: true,
-//   dateFormatter: date => `Last updated at ${date.toDateString()}`,
-//   footer: (
-//     <small style={{ display: 'block', marginTop: '8rem' }}>
-//       <abbr
-//         title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License."
-//         style={{ cursor: 'help' }}
-//       >
-//         CC BY-NC 4.0
-//       </abbr>{' '}
-//       {new Date().getFullYear()} © Shu Ding.
-//       <a href="/feed.xml">RSS</a>
-//       <style jsx>{`
-//         a {
-//           float: right;
-//         }
-
-//         @media screen and (max-width: 480px) {
-//           article {
-//             padding-top: 2rem;
-//             padding-bottom: 4rem;
-//           }
-//         }
-//       `}</style>
-//     </small>
-//   )
-// }
